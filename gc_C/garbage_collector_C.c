097acdcd7c23a74d05fb421e92bafb0a58f08e28
RadoslavSV <73000513+RadoslavSV@users.noreply.github.com>
2023-02-05 22:42:50 +0200

    Add files via upload
    Add .c file with first steps - a skeleton code for implementing a garbage collector algorithm in C.

---
+++ gc_C/garbage_collector_C.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STACK 256
+#define INIT_OBJ_NUM_MAX 8
+
+typedef enum {
+    OBJ_INT,
+    OBJ_PAIR
+} ObjectType;
+
+typedef struct sObject {
+    ObjectType type;
+    unsigned char marked;
+    struct sObject *next;
+    union {
+        int value;
+        struct {
+            struct sObject *head;
+            struct sObject *tail;
+        };
+    };
+} Object;
+
+typedef struct {
+    Object* stack[MAX_STACK];
+    int stackSize;
+    Object* firstObject;
+    int numObjects;
+    int maxObjects;
+} VM;
+
+void assert(int condition, const char* message) {
+    if (!condition) {
+        printf("%s\n", message);
+        exit(1);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+
+    // TO DO
+
+    return 0;
+}
